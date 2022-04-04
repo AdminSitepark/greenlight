3e1d8880fcacf0ea4864e6cc79c8a6fad126b897
root <root@bbb.sitepark.com>
2022-04-04 14:55:22 +0200

    April 2022

--- db/migrate/20211215001240_add_login_attempt_to_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddLoginAttemptToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :failed_attempts, :integer
-    add_column :users, :last_failed_attempt, :datetime
-  end
-end
